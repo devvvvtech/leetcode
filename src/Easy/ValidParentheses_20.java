b1aad2e62e7e765edb805ec41e9187467f55d6f5
Devender Nainwal <devendernainwal15@gmail.com>
2025-07-18 21:11:05 +0530

    add leetcode20 file

---
+++ src/Easy/ValidParentheses_20.java
@@ -0,0 +1,39 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses_20 {
+    public static void main(String[] args) {
+        String s = "()";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap();
+        map.put(']', '[');
+        map.put('}', '{');
+        map.put(')', '(');
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(stack.isEmpty() && ch == '}' || ch == ')' || ch == ']' ) {
+                return false;
+            }
+            else if(ch == '}' || ch == ')' || ch == ']') {
+                if(stack.peek().equals(map.get(ch))) {
+                    stack.pop();
+                }
+            }
+            else {
+                stack.push(ch);
+            }
+        }
+
+        if(stack.isEmpty()) return true;
+        else return false;
+    }
+}
